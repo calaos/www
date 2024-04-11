e5b5c3c4149d261dfdc349aad3a4b90e873eb319
raoulh <raoul.hecky@gmail.com>
2024-04-11 10:55:37 +0200

    fix multiple redirect

--- index.php
+++ index.php
@@ -3,11 +3,11 @@ $lang = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);
 switch ($lang)
 {
     case "fr":
-		header("location: /fr");
+		header("location: fr/");
         break;
 	default:
     case "en":
-        header("location: /en");
+        header("location: en/");
         break;
 }
 ?>
